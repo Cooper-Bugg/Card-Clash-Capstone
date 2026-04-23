036cbeabf4797f7610c97ac7ed97d52045e97dbe
Napkin21 <trevorwm@yahoo.com>
2026-04-22 22:52:23 -0500

    Update dbConnect.js

--- Backend/dbConnect.js
+++ Backend/dbConnect.js
@@ -27,10 +27,11 @@ const mysql = require('mysql2/promise');
 require("dotenv").config({ path: path.join(__dirname, "../.env") });
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,//'localhost',//
-    user: process.env.DB_USER,//'root',//
-    password: process.env.DB_PASSWORD,//'K#33sh8r/s1ng3n71t6',//
-    database: process.env.DB_DATABASE,//'cardclash_db'//
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
     ssl: { rejectUnauthorized: false }
 });
 
